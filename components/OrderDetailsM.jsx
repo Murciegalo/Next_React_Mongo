f4198d522f0f31d680270f905884eed487b95667
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-06-12 11:03:22 +0200

    Order_for cashPayments done

--- components/OrderDetailsM.jsx
+++ components/OrderDetailsM.jsx
@@ -1,23 +1,50 @@
 import { useState } from 'react';
 import styles from '../styles/OrderDetailM.module.css';
 
-const OrderDetailsM = () => {
+const OrderDetailsM = ({ total, createOrder }) => {
   const [customer, setCustomer] = useState('');
   const [address, setAddress] = useState('');
 
+  const handleClick = () => {
+    createOrder({ customer, address, total, method: 0 });
+  };
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Due payment of 12$ upon delivery</h1>
         <div className={styles.item}>
-          <label className={styles.label}>Fullname</label>
+          <label className={styles.label}>Your fullname</label>
           <input
             placeholder='Jhon Doe'
             type='text'
             className={styles.input}
             onChange={(e) => setCustomer(e.target.value)}
+            required
           />
         </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Phone Number</label>
+          <input
+            placeholder='+1 524 452 12'
+            type='text'
+            className={styles.input}
+            onChange={(e) => setCustomer(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Address</label>
+          <input
+            placeholder='Kilimanjaro, top 1degrees'
+            type='text'
+            className={styles.input}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </div>
+        <button className={styles.button} onClick={handleClick}>
+          Order
+        </button>
       </div>
     </div>
   );
--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   }
   if (method === 'PUT') {
     try {
-      const order = await Order.create(req.body);
+      const order = await Order.findByIdAndUpdate(req.body);
       return res.status(200).json(order);
     } catch (err) {
       return res.status(500).json(err);
