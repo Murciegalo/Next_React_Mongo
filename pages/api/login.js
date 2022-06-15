cf1e242ecebdd50c045d307371daef6bd91ec602
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-06-15 09:00:44 +0200

    EMULATING login for adminPg redirect/auth functionality

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,6 +3,15 @@ import cookie from 'cookie';
 const handler = (req, res) => {
   if (req.method === 'POST') {
     const { username, password } = req.body;
+    if (
+      username === process.env.USERNAME &&
+      password === process.env.PASSWORD
+    ) {
+      res.setHeaders(
+        'Set-Cookie',
+        cookie.serialize('token', process.env.TOKEN)
+      );
+    }
   }
 };
 
